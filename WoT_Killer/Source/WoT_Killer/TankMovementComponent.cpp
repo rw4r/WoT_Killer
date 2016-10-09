faff36b54d331826eefefe7f18219d0270dc5459
HUKBALAHAP\F <aa@gmail.com>
2016-10-09 21:51:33 +0800

    Vector Product

--- WoT_Killer/Source/WoT_Killer/TankMovementComponent.cpp
+++ WoT_Killer/Source/WoT_Killer/TankMovementComponent.cpp
@@ -16,25 +16,36 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s moving with a speed of %s"), *TankName, *MoveVelocityString);
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+
+	auto TurnThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;//Z component vector
+
+	IntendAccelerate(ForwardThrow);
+	IntendTurnRight(TurnThrow);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s moving with a speed of %s"), *TankName, *MoveVelocityString);
 }
 
 void UTankMovementComponent::IntendAccelerate(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
 
 void UTankMovementComponent::IntendTurnLeft(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(-Throw);
 	RightTrack->SetThrottle(Throw);
 }
